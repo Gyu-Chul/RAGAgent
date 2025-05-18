311ade342917b4f93fae1af2da04ec8bceffea50
shinminchul <su99king@gmail.com>
2025-05-18 19:57:57 +0900

    fix: flag 파일 생성& 이미지 소스 제거

--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -5,13 +5,12 @@ def render():
         with ui.column().style('align-items:center; padding:30px; border:2px solid #ccc; border-radius:15px; box-shadow:0 0 10px #ccc; min-width:350px;'):
             # 로고 & 타이틀
             with ui.row().style('align-items:center; margin-bottom:40px;'):
-                ui.image('./static/gitai_logo.png').style('width:60px; height:60px; margin-right:10px;')
                 ui.label('GitAI').style('font-size:28px; font-weight:bold;')
 
             ui.label('연결된 레포지토리').style('font-size:22px; font-weight:bold; margin-bottom:20px;')
 
             # 더미 버튼
-            for repo in ['hab', 'JEYSport']:
+            for repo in ['hab', 'JEYSport',"TEST"]:
                 ui.button(
                     repo.upper(),
                     on_click=lambda r=repo: ui.navigate.to(f'/project/{r}')
