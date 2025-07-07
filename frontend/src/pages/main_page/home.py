4233aea3d4eee8f0bc778e0e15f292b61e142221
su9king <su99king@gmail.com>
2025-07-07 16:52:08 +0900

    fix : 연결된 레포지토리 메인 페이지에 노출됨

--- frontend/src/pages/main_page/home.py
+++ frontend/src/pages/main_page/home.py
@@ -3,8 +3,14 @@
 from nicegui import ui
 from functools import partial
 from src.components.main_page.api import gitclone
+import os
 
 
+current_file_path = os.path.abspath(__file__)
+
+#직관적인 경로체크 코드 수정 필요
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))))
+parsed_repo_path = os.path.join(project_root, 'git-agent3', 'parsed_repository')
 
 def logo_header():
     with ui.row().style('align-items:center; margin-bottom:40px;'):
@@ -28,19 +34,22 @@ def connect_repo():
 def render_home():
     with ui.row().style('height:100vh; width:100vw; justify-content:center; align-items:center;'):
         with ui.column().style('align-items:center; padding:30px; border:2px solid #ccc; border-radius:15px; box-shadow:0 0 10px #ccc; min-width:350px;'):
-            # 로고 & 타이틀
+            # 로고 & 타이틀ㄴ
             logo_header()
 
-            # 연결된 레포지토리 타이틀
-            test = []
+            parsed_repo = []
+            if os.path.exists(parsed_repo_path) and os.path.isdir(parsed_repo_path):
+                parsed_repo = [name for name in os.listdir(parsed_repo_path) if
+                               os.path.isdir(os.path.join(parsed_repo_path, name))]
 
-            if test:
+            print(parsed_repo)
+            if parsed_repo:
                 ui.label('연결된 레포지토리').style('font-size:22px; font-weight:bold; margin-bottom:20px;')
             else:
                 ui.label('연결된 레포지토리가 존재하지 않습니다.').style('font-size:22px; font-weight:bold; margin-bottom:20px;')
 
             # 더미 레포 버튼
-            for repo in test:
+            for repo in parsed_repo:
                 ui.button(repo.upper(), on_click=lambda r=repo: ui.navigate.to(f'/project/{r}')).style('width:250px; height:50px; margin:10px; font-size:16px;')
 
             # NEW CONNECT 버튼
